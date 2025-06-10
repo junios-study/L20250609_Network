c5070416a44d8d1724854e739a5a8daab454cb9b
junios-study <junios@naver.com>
2025-06-10 10:01:44 +0900

    접속 제어

--- Source/L20250609_Network/Lobby/LobbyGM.cpp
+++ Source/L20250609_Network/Lobby/LobbyGM.cpp
@@ -3,3 +3,23 @@
 
 #include "LobbyGM.h"
 
+void ALobbyGM::PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage)
+{
+	Super::PreLogin(Options, Address, UniqueId, ErrorMessage);
+
+	//ErrorMessage = TEXT("get out here.");
+
+	UE_LOG(LogTemp, Warning, TEXT("PreLogin : %s %s"), *Options, *Address);
+}
+
+APlayerController* ALobbyGM::Login(UPlayer* NewPlayer, ENetRole InRemoteRole, const FString& Portal, const FString& Options, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Login : %d %s"), InRemoteRole, *Options) ;
+
+	return Super::Login(NewPlayer, InRemoteRole, Portal, Options, UniqueId, ErrorMessage);
+}
+
+void ALobbyGM::PostLogin(APlayerController* NewPlayer)
+{
+	UE_LOG(LogTemp, Warning, TEXT("PostLogin : %s"), *NewPlayer->GetName());
+}
--- Source/L20250609_Network/Lobby/LobbyGM.h
+++ Source/L20250609_Network/Lobby/LobbyGM.h
@@ -13,5 +13,13 @@ UCLASS()
 class L20250609_NETWORK_API ALobbyGM : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+public:
+	virtual void PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage) override;
+
+	virtual APlayerController* Login(UPlayer* NewPlayer, ENetRole InRemoteRole, const FString& Portal, const FString& Options, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage) override;
+
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+
+
+
 };
