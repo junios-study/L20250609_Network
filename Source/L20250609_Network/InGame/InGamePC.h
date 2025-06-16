605d96aef2be7a2ef330c144bb4c8871b445e1fc
junios-study <junios@naver.com>
2025-06-16 09:22:30 +0900

    InGame 시스템 세팅

---
+++ Source/L20250609_Network/InGame/InGamePC.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "InGamePC.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class L20250609_NETWORK_API AInGamePC : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
